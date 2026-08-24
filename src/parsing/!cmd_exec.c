87dfe10c959ea20287a832b8886d0f6357397e2d
Lucile Tournier <ltournie@student.42.fr>
2026-08-24 17:16:12 +0200

    added cmd_exec pipe functions, TBC

--- src/parsing/!cmd_exec.c
+++ src/parsing/!cmd_exec.c
@@ -38,80 +38,105 @@ char	*get_path(char **envp, char *command)
 	return (NULL);
 }
 
-void	set_path(char *arg, char **envp, t_pipex *p)
+void	set_path(char *arg, char **envp, t_cmd *cmds)
 {
 	char	*str;
 	char	*find_path;
 
-	p->path = NULL;
-	if (p->exec[0] == NULL)
+	cmds->path = NULL;
+	if (cmds->args[0] == NULL)
 		return ;
-	if (access(arg, F_OK) == 0)
-		p->path = strdup(arg);
+	if (access(arg, F_OK) == 0) // si le file envoyer == path
+		cmds->path = strdup(arg);
 	else
 	{
-		str = ft_strjoin("/\0", p->exec[0]);
+		str = ft_strjoin("/\0", cmds->args[0]);
 		find_path = get_path(envp, str);
 		if (find_path != NULL)
-		{
-			p->path = find_path;
-			free(str);
-			if (p->path != NULL)
-				return ;
-		}
+			cmds->path = find_path;
 		free(str);
 	}
 }
 
-static void	check_exec2(t_shell *p, int fd)
-{
-	if (fd < 0)
-	{
-		if (ft_strlen(p->argv[4]) == 0)
-			print_error(p->exec[0], &(p->argv[0][2]));
-		else
-			print_error(p->argv[4], &(p->argv[0][2]));
-		if (p->path != NULL)
-			(free_struct(p), exit(1));
-		else
-			(free_tab(p->exec), exit(1));
-	}
-	if (p->path == NULL || p->exec[0] == NULL)
-	{
-		print_error_not_found(p->argv[3], &(p->argv[0][2]));
-		(free_tab(p->exec), exit(127));
-	}
-	if (access(p->path, F_OK) < 0)
-	{
-		print_error(p->argv[3], &(p->argv[0][2]));
-		(free_tab(p->exec), exit(1));
-	}
-}
+// static void	check_exec(t_cmd *cmds, int fd)
+// {
+// 	if (fd < 0)
+// 	{
+// 		if (ft_strlen(cmds->argv[4]) == 0)
+// 			print_error(cmds->exec[0], &(cmds->argv[0][2]));
+// 		else
+// 			print_error(cmds->argv[4], &(cmds->argv[0][2]));
+// 		if (cmds->path != NULL)
+// 			(free_struct(cmds), exit(1));
+// 		else
+// 			(free_tab(cmds->exec), exit(1));
+// 	}
+// 	if (cmds->path == NULL || cmds->exec[0] == NULL)
+// 	{
+// 		print_error_not_found(cmds->argv[3], &(cmds->argv[0][2]));
+// 		(free_tab(cmds->exec), exit(127));
+// 	}
+// 	if (access(cmds->path, F_OK) < 0)
+// 	{
+// 		print_error(cmds->argv[3], &(cmds->argv[0][2]));
+// 		(free_tab(cmds->exec), exit(1));
+// 	}
+// }
 
-void	exec1(t_shell *p)
+void type_in(t_cmd *cmds, char **envp)
 {
-	int	fd;
+	int fd;
 
-	close(p->fd_pipe[0]);
-	fd = open(p->argv[1], O_RDONLY);
-	check_exec1(p, fd);
+	close(cmds->fd_pipe[0]);
+	fd = open(cmds->redirs->target, O_RDONLY);
+	// check_exec(cmds, fd);
 	(dup2(fd, STDIN_FILENO), close(fd));
-	(dup2(p->fd_pipe[1], STDOUT_FILENO), close(p->fd_pipe[1]));
-	p->exit_status1 = execve(p->path, p->exec, p->envp);
-	print_error(p->argv[2], &(p->argv[0][2]));
-	(free_struct(p), exit(127));
+	(dup2(cmds->redirs->fd_pipe[1], STDOUT_FILENO), close(cmds->fd_pipe[1]));
+	cmds->exit_status = execve(cmds->path, cmds->args, envp);
+	print_error(cmds->args[0], &(cmds->argv[0][2]));
+	(free_struct(cmds), exit(127));
 }
 
-void	exec2(t_shell *p)
+void type_mid(t_cmd *cmds, char **envp)
+{
+	// check_exec(cmds, fd);
+	(dup2(cmds->redirs->fd_pipe[0], STDIN_FILENO), close(fd));
+	(dup2(cmds->redirs->fd_pipe[0], STDOUT_FILENO), close(fd));
+	cmds->exit_status = execve(cmds->path, cmds->args, envp);
+	print_error(cmds->args[0], &(cmds->argv[0][2]));
+	(free_struct(cmds), exit(127));
+}
+
+void type_out(t_cmd *cmds, char **envp)
+{
+	int fd;
+
+	close(cmds->fd_pipe[1]);
+	fd = open(cmds->redirs->target, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+	// check_exec(cmds, fd);
+	(dup2(cmds->fd_pipe[0], STDIN_FILENO), close(cmds->fd_pipe[0]));
+	(dup2(fd, STDOUT_FILENO), close(fd));
+	cmds->exit_status = execve(cmds->path, cmds->args, envp);
+	print_error(cmds->args[0], &(cmds->argv[0][2]));
+	(free_struct(cmds), exit(127));
+}
+
+void	exec(t_cmd *cmds, char **envp)
 {
 	int	fd;
 
-	close(p->fd_pipe[1]);
-	fd = open(p->argv[4], O_WRONLY | O_TRUNC | O_CREAT, 0644);
-	check_exec2(p, fd);
-	(dup2(p->fd_pipe[0], STDIN_FILENO), close(p->fd_pipe[0]));
-	(dup2(fd, STDOUT_FILENO), close(fd));
-	execve(p->path, p->exec, p->envp);
-	print_error(p->argv[3], &(p->argv[0][2]));
-	(free_struct(p), exit(127));
+	set_path(cmds->args[0], envp, cmds);
+	//fonction checkpipe pour savoir si la pipe est une entree/sortie/pipe to pipe pour set les fd
+	if (cmds->redirs->type == TOKEN_PIPE_IN)
+		type_in(cmds, envp);
+	else if (cmds->redirs->type == TOKEN_PIPE_MID)
+		type_mid(cmds, envp);
+	else if (cmds->redirs->type == TOKEN_PIPE_OUT)
+		type_out(cmds, envp);
+	else
+	{
+	cmds->exit_status = execve(cmds->path, cmds->args, envp);
+	print_error(cmds->args[0], &(cmds->argv[0][2]));
+	(free_struct(cmds), exit(127));
+	}
 }
