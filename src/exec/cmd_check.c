5c4b48e6ddb4863021728776b5c57ba390585183
ltournie <ltournie@f1br5s4.paris.42.school>
2026-08-31 14:58:13 +0200

    working cd

--- src/exec/cmd_check.c
+++ src/exec/cmd_check.c
@@ -3,8 +3,11 @@
 int check_cmd(t_cmd *cmds)
 {
 	if (ft_strncmp (cmds->args[0], "cd\0", 3) == 0)
+	{
+		printf("in cd\n");
 		return (cmd_cd(cmds), 0);
-	if (ft_strncmp (cmds->args[0], "pwd\0", 3) == 0)
+	}
+	if (ft_strncmp (cmds->args[0], "pwd\0", 4) == 0)
 		return (cmd_pwd(), 0);
 	else
 		return (1);
