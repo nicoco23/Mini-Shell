50edc218be4a88dfee1f511f9a4db0fe9bdad192
Lucile Tournier <ltournie@student.42.fr>
2026-09-08 12:25:58 +0200

    fix fds

--- src/exec/cmd_check.c
+++ src/exec/cmd_check.c
@@ -39,17 +39,17 @@ int	run_builtin(t_shell *shell, t_cmd *cmd)
 {
 	int	ret;
 
-	shell->saved_in = dup(STDIN_FILENO);
-	shell->saved_out = dup(STDOUT_FILENO);
+	// shell->saved_in = dup(STDIN_FILENO);
+	// shell->saved_out = dup(STDOUT_FILENO);
 	if (apply_redirs(cmd))
 		ret = 1;
 	else
 		ret = dispatch_builtin(shell, cmd);
-	dup2(shell->saved_in, STDIN_FILENO);
-	dup2(shell->saved_out, STDOUT_FILENO);
-	close(shell->saved_in);
-	close(shell->saved_out);
-	shell->saved_in = -1;
-	shell->saved_out = -1;
+	// dup2(shell->saved_in, STDIN_FILENO);
+	// dup2(shell->saved_out, STDOUT_FILENO);
+	// close(shell->saved_in);
+	// close(shell->saved_out);
+	// shell->saved_in = -1;
+	// shell->saved_out = -1;
 	return (ret);
 }
